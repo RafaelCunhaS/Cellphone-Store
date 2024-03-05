ca793e2b0560a34a8f17a19f09f566df7af6ac22
Rafael Santos <rafaelcs_94@hotmail.com>
2024-03-05 17:04:43 -0300

    fix: use dialectModule

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 require('dotenv').config();
 
 const config = {
@@ -7,6 +8,7 @@ const config = {
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   dialect: 'postgres',
+  dialectModule: require('pg'),
   dialectOptions: {
     ssl: {
       require: true,
