aaeef71ec0c125c7493a25b4be2ae2f99bcbfff7
Rafael Santos <rafaelcs_94@hotmail.com>
2024-03-06 19:08:58 -0300

    feat: create home page

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import { MdOutlinePhoneIphone } from 'react-icons/md';
+import styles from './styles.module.scss';
+import CustomInput from '../../components/Input';
+import { Button } from '../../components/Button';
+import { api } from '../../services/api';
+import { Link, useNavigate } from 'react-router-dom';
+
+export function Home() {
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState('name');
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState('');
+  const navigate = useNavigate();
+
+  async function getProducts() {
+    const { data } = await api.get(`/cellphone?category=${category}&search=${search}&sort=${sort}`);
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    getProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search, sort]);
+
+  function handleSelect(e) {
+    setCategory(e.currentTarget.value);
+  }
+
+  function handleSort(e) {
+    setSort(e.currentTarget.value);
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1>Cellphone Store</h1>
+
+      <div className={styles.filters}>
+        <div className={styles.searchContainer}>
+          <div>
+            <label htmlFor="search" className="">
+              Filter by:
+            </label>
+            <select className="" name="search" id="search" onChange={handleSelect}>
+              <option defaultChecked value="name">
+                Name
+              </option>
+              <option value="brand">Brand</option>
+              <option value="model">Model</option>
+              <option value="color">Color</option>
+            </select>
+          </div>
+
+          <div className={styles.searchInput}>
+            <CustomInput Icon={FiSearch} autoComplete="off" onChange={(e) => setSearch(e.target.value.toLowerCase())} />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="sort" className="">
+            Sort price by:
+          </label>
+          <select className="" name="sort" id="sort" onChange={handleSort}>
+            <option defaultChecked value="asc">
+              Ascending
+            </option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+      </div>
+
+      <Button
+        title="Add Cellphone"
+        icon={<MdOutlinePhoneIphone />}
+        type="button"
+        onClick={() => navigate('/add-cellphone')}
+      />
+
+      <div className={styles.cardsContainer}>
+        {products &&
+          products.map((product) => (
+            <Link key={product.id} className={styles.cellphoneCard} to={`/cellphone/${product.id}`}>
+              <h3>{product.name}</h3>
+              <hr />
+              <p>Brand: {product.brand}</p>
+              <p>Model: {product.model}</p>
+              <p>Price: ${product.price}</p>
+              <p>Color: {product.color}</p>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+}
