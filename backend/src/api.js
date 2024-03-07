da3ecf3b738e6b4d70df9a468f4d3db725358d4a
Rafael Santos <rafaelcs_94@hotmail.com>
2024-03-07 17:01:49 -0300

    feat: Create swagger

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,9 +1,11 @@
 require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 const userRouter = require('./routes/userRouter');
 const cellphoneRouter = require('./routes/cellphoneRouter');
 const errorMiddleware = require('./middlewares/error');
+const swaggerDocs = require('../swagger_output.json');
 
 const app = express();
 app.use(express.json());
@@ -12,6 +14,8 @@ app.use(cors());
 app.use('/user', userRouter);
 
 app.use('/cellphone', cellphoneRouter);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(errorMiddleware);
 
