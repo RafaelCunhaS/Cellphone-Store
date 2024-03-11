a0ff7b6e070611c167e2c18946392368eb2abc60
Rafael Santos <rafaelcs_94@hotmail.com>
2024-03-10 21:46:19 -0300

    build: downgrade swagger-ui library version

--- backend/src/api.js
+++ backend/src/api.js
@@ -2,27 +2,21 @@ require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const path = require('path');
 const userRouter = require('./routes/userRouter');
 const cellphoneRouter = require('./routes/cellphoneRouter');
 const errorMiddleware = require('./middlewares/error');
 const swaggerDocs = require('../swagger_output.json');
 
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/user', userRouter);
 
 app.use('/cellphone', cellphoneRouter);
 
-app.use(
-  '/api-docs',
-  express.static('node_modules/swagger-ui-dist/', { index: false }),
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs)
-);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL }));
 
 app.use(errorMiddleware);
 
